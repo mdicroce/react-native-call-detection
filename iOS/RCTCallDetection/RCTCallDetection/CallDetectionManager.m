060262d02c69fad091a3563ae19f563d6c3852a6
Matias Di Croce <matias.dicroce@globallogic.com>
2026-02-27 15:32:45 -0300

    Se agrega la funcionalidad que devuelve el estado actual de llamada al iniciar el listener

--- iOS/RCTCallDetection/RCTCallDetection/CallDetectionManager.m
+++ iOS/RCTCallDetection/RCTCallDetection/CallDetectionManager.m
@@ -8,57 +8,121 @@
 //
 
 #import "CallDetectionManager.h"
-@import CallKit;
+#import <CallKit/CallKit.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-typedef void (^CallBack)();
-@interface CallDetectionManager()
+@interface CallDetectionManager () <CXCallObserverDelegate>
 
-@property(strong, nonatomic) RCTResponseSenderBlock block;
-@property(strong, nonatomic) CXCallObserver* callObserver;
+@property(strong, nonatomic) CXCallObserver *callObserver;
+@property(assign, nonatomic) BOOL hasSentInitialState;
+@property(assign, nonatomic) BOOL isObserving;
 
 @end
 @implementation CallDetectionManager
 
 - (NSArray<NSString *> *)supportedEvents {
-    return @[@"PhoneCallStateUpdate"];
+  return @[ @"PhoneCallStateUpdate" ];
 }
 
 + (BOOL)requiresMainQueueSetup {
-    return YES;
+  return YES;
 }
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(addCallBlock:(RCTResponseSenderBlock) block) {
-    // Setup call tracking
-    self.block = block;
-    self.callObserver = [[CXCallObserver alloc] init];
-    __typeof(self) weakSelf = self;
-    [self.callObserver setDelegate:weakSelf queue:nil];
-}
-
 RCT_EXPORT_METHOD(startListener) {
-    // Setup call tracking
+  // Setup call tracking
+  if (self.callObserver == nil) {
     self.callObserver = [[CXCallObserver alloc] init];
     __typeof(self) weakSelf = self;
     [self.callObserver setDelegate:weakSelf queue:nil];
+  }
 }
 
 RCT_EXPORT_METHOD(stopListener) {
-    // Setup call tracking
-    self.callObserver = nil;
+  // Setup call tracking
+  self.callObserver = nil;
+}
+
+RCT_EXPORT_METHOD(getCurrentState:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+  // Ensure we have an observer to read current calls
+  if (self.callObserver == nil) {
+    self.callObserver = [[CXCallObserver alloc] init];
+    __typeof(self) weakSelf = self;
+    [self.callObserver setDelegate:weakSelf queue:nil];
+  }
+
+  @try {
+    NSString *state = @"Disconnected";
+    if (self.callObserver.calls.count > 0) {
+      // Use the most recent call for state reporting
+      CXCall *call = [self.callObserver.calls lastObject];
+      if (call.hasEnded == true) {
+        state = @"Disconnected";
+      } else if (call.hasConnected == true) {
+        state = @"Connected";
+      } else if (call.isOutgoing == true) {
+        state = @"Dialing";
+      } else {
+        state = @"Incoming";
+      }
+    }
+    resolve(state);
+  } @catch (NSException *exception) {
+    reject(@"call_state_error", exception.reason, nil);
+  }
+}
+
+RCT_EXPORT_METHOD(requestCurrentState) {
+  // Ensure we have an observer to read current calls
+  if (self.callObserver == nil) {
+    self.callObserver = [[CXCallObserver alloc] init];
+    __typeof(self) weakSelf = self;
+    [self.callObserver setDelegate:weakSelf queue:nil];
+  }
+  [self reportCurrentState];
+}
+
+- (void)startObserving {
+  self.isObserving = YES;
+  // Ensure observer exists
+  if (self.callObserver == nil) {
+    self.callObserver = [[CXCallObserver alloc] init];
+    __typeof(self) weakSelf = self;
+    [self.callObserver setDelegate:weakSelf queue:nil];
+  }
+  // Emit the initial state now that JS is listening
+  [self reportCurrentState];
+}
+
+- (void)stopObserving {
+  self.isObserving = NO;
 }
 
 - (void)callObserver:(CXCallObserver *)callObserver callChanged:(CXCall *)call {
-    if (call.hasEnded == true) {
-      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Disconnected"];
-    } else if (call.hasConnected == true) {
-      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Connected"];
-    } else if (call.isOutgoing == true) {
-      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Dialing"];
-    } else {
-      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Incoming"];
+  if (!self.isObserving) { return; }
+  if (call.hasEnded == true) {
+    [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Disconnected"];
+  } else if (call.hasConnected == true) {
+    [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Connected"];
+  } else if (call.isOutgoing == true) {
+    [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Dialing"];
+  } else {
+    [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Incoming"];
+  }
+}
+
+- (void)reportCurrentState {
+  if (!self.isObserving) { return; }
+  if (self.callObserver.calls.count > 0) {
+    for (CXCall *call in self.callObserver.calls) {
+      [self callObserver:self.callObserver callChanged:call];
     }
+  } else {
+    [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Disconnected"];
+  }
 }
 
 @end
+
